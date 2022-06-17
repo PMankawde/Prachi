6c715075d352511b32151eeacbab4f1fee2b9655
PMankawde <prachimankawde@gmail.com>
2022-06-17 20:49:36 +0530

    initial commit

---
+++ src/practicing/First.java
@@ -0,0 +1,21 @@
+package practicing;
+public class First {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MyName myname= new MyName();
+		System.out.println(myname.google());
+	}
+
+}
+ class MyName{
+	MyName(){
+		System.out.println("Hello Prachi");
+	}
+
+	public int google() {
+		int i=999;
+		return i;
+	}
+	
+}
